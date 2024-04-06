e74790bbfc314ef7affd465f607bf8bedd60c01a
jharrymoore <jharrymoore@btinternet.com>
2024-04-06 20:30:10 +0100

    update install instructions

--- mace-install.sh
+++ mace-install.sh
@@ -3,6 +3,6 @@ mamba env create -f mace-openmm.yml
 
 conda activate mace-openmm
 
-pip install git+https://github.com/ACEsuit/mace.git
+pip install git+https://github.com/jharrymoore/mace.git@softcore
 pip install git+https://github.com/jharrymoore/openmm-ml.git@ml_alchemy
 pip install .
