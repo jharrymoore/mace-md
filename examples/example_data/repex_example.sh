331b247d100a75733e526e83e8a75917cb07cfbf
Harry Moore <31904583+jharrymoore@users.noreply.github.com>
2024-05-29 15:38:24 +0100

    Update repex_example.sh

--- examples/example_data/repex_example.sh
+++ examples/example_data/repex_example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-srun -n 12 --gpu-bind=map_gpu:0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3 mace-md -f /path/to/input_file.pdb\
+srun -n 12 --gpu-bind=map_gpu:0,0,0,1,1,1,2,2,2,3,3,3 mace-md -f /path/to/input_file.pdb\
   --ml_mol /path/to/input_file.pdb \
   --resname "LIG"\
   --output_dir "./16_reps_production" \
