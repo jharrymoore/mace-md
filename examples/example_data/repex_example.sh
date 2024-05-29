9d4e94e33c613b41112144df8618c998bc0702f7
Harry Moore <31904583+jharrymoore@users.noreply.github.com>
2024-05-29 15:38:09 +0100

    Update repex_example.sh

--- examples/example_data/repex_example.sh
+++ examples/example_data/repex_example.sh
@@ -7,7 +7,7 @@ srun -n 12 --gpu-bind=map_gpu:0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3 mace-md -f /path/t
   --pressure 1.0 \
   --decouple \
   --restart \
-  --replicas 16 \
+  --replicas 12 \
   --log_level DEBUG \
   --nl "nnpops" \
   --run_type "repex" \
@@ -17,4 +17,3 @@ srun -n 12 --gpu-bind=map_gpu:0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3 mace-md -f /path/t
   --steps 5000 \
   --model_path "/path/to/MACE.model" \
   --steps_per_iter 1000 \
-  --lambda_schedule "[0.,   0.04, 0.08     , 0.12, 0.16,   0.20, 0.2425, 0.28, 0.35, 0.4 , 0.52, 0.64, 0.76, 0.88, 0.95, 1.   ]" \
